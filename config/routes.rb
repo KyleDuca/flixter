973409a13adddbcbfea46fb36aaf43a8fe464e94
Kyle Duca <kyleduca@gmail.com>
2017-07-07 04:48:00 +0000

    Added page to submit new courses and an instructor page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'static_pages#index'
-  resources :courses
+  # resources :courses
   resources :users, only: :show
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end
