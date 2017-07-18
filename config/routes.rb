ec899c3ba3781ed0d5c6aa5894e34f9415d5e3f2
Kyle Duca <kyleduca@gmail.com>
2017-07-18 00:48:03 +0000

    Added model, linked lessons with sections, updated students view.

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # resources :courses
   resources :users, only: :show
   resources :courses, only: [:index, :show]
+  resources :lessons, only: [:show]
   namespace :instructor do
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
