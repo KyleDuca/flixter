3a848c604f248f164547a438a4de004c6d95862a
Kyle Duca <kyleduca@gmail.com>
2017-07-23 18:43:33 +0000

    Added enrollment to courses

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   devise_for :users
   root 'static_pages#index'
+  resources :courses, only: [:index, :show] do
+    resources :enrollments, only: :create
+  end
   # resources :courses
   resources :users, only: :show
   resources :courses, only: [:index, :show]
