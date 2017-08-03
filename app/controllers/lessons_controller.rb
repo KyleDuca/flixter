f2d253986ec402438eeeba09ac6e8fa8392eb9a4
Kyle Duca <kyleduca@gmail.com>
2017-08-03 08:18:33 +0000

    set up payment processing with stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,19 @@
 class LessonsController < ApplicationController
-  def show
+  before_action :authenticate_user!
+  before_action :require_user_enrollment_for_lesson, only: [:show]
 
+  def show
   end
 
   private
-
+  
+  def require_user_enrollment_for_lesson
+    if current_lesson.section.course.enrollments.include?(current_user) != true
+    # if current_user.enrolled_in?(current_lesson.section.course) != true || current_user.enrollments.nil? == true 
+      redirect_to course_path(current_lesson.section.course), alert: 'Please Enroll to Access Lessons'
+    end
+  end
+ 
   helper_method :current_lesson
 
   def current_lesson
